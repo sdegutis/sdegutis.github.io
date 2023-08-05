28cd67ab9da72e44af46f0e0a41691d84355c8e2
Steven Degutis <sbdegutis@gmail.com>
2023-08-05 16:05:48 -0500

    Article index prep.

--- new/lib/nav-links.js
+++ new/lib/nav-links.js
@@ -1,7 +1,7 @@
 export const navLinks = `
   <a href='/'>TSP</a>
-  <a href='/articles.html'>Articles</a>
-  <a href='/portfolio.html'>Portfolio</a>
+  <a href='/articles'>Articles</a>
+  <a href='/portfolio'>Portfolio</a>
   <a href='https://github.com/sdegutis/'>GitHub</a>
   <a href='sbdegutis@gmail.com'>Email</a>
 `;
