e9154ad9cffb70a235e74c8bc393f309cdc72d53
Steven Degutis <sbdegutis@gmail.com>
2023-08-05 14:09:54 -0500

    Experimenting with articles.

--- new/lib/common.js
+++ new/lib/common.js
@@ -1,3 +1,8 @@
+import './footer.js';
 import './head.js';
 import './navbar.js';
-import './footer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const title = document.querySelector('h1');
+  document.title += ' - ' + title.textContent;
+});
