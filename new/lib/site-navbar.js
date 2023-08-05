2d9a5e2fba0ec6e1f85283c3e913594d8985d7b2
Steven Degutis <sbdegutis@gmail.com>
2023-08-05 13:33:53 -0500

    Got navbar working on mobile. Neat.

--- new/lib/site-navbar.js
+++ new/lib/site-navbar.js
@@ -16,9 +16,34 @@ class SiteNavbar extends HTMLElement {
         <a href='sbdegutis@gmail.com'>Email</a>
       </nav>
       <link rel='stylesheet' href='/lib/navbar.css'>
+      <div id='site-navbar-mobile'>
+        <a href='/'>The Software Philosopher</a>
+        <a href='#' class='site-navbar-togglers'>☰</a>
+      </div>
     `;
 
+    const shadow = frag.content.getElementById('site-navbar-shadow');
+    const navbar = frag.content.getElementById('site-navbar');
+    const togglers = frag.content.querySelectorAll('.site-navbar-togglers');
+
+    for (const button of togglers) {
+      button.onclick = (e) => {
+        e.preventDefault();
+        toggleMenu();
+      };
+    }
+
+    shadow.onclick = () => {
+      toggleMenu();
+    };
+
+    function toggleMenu() {
+      navbar.classList.toggle('open');
+      shadow.classList.toggle('open');
+    }
+
     this.shadowRoot.append(frag.content);
+
   }
 }
 
